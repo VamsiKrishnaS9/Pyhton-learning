94a2ea72ccb1d1f54860897492bb3186465bf171
sonuk9 <krishnahm312@gmail.com>
2021-04-26 23:33:17 +0530

    new commit

--- sample-doc/sqrt.py
+++ sample-doc/sqrt.py
@@ -4,6 +4,6 @@ a=int(input("Enter the A value\n"))
 b=int(input("Enter the B value\n"))
 c=a*a+2*a*b+b*b
 print("a+b whole square ",c)
-c+a*a*a+3*a*a*b+3*a*b*b+b*b*b
+c=a*a*a+3*a*a*b+3*a*b*b+b*b*b
 print("a+b whole cube ",c)
 
