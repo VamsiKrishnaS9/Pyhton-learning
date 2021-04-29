8a9091c77f9d6ed7df47d2c062269b757c734f40
sonuk9 <krishnahm312@gmail.com>
2021-04-29 11:59:58 +0530

    added new file

---
+++ sample-doc/num-range.py
@@ -0,0 +1,9 @@
+#this program will print numbers from 1 to 100
+
+for i in range(1, 101):
+    print(i)
+
+'''
+here the for loop prints the numbers from 1 to 100 using range()function
+and i the variable
+'''
