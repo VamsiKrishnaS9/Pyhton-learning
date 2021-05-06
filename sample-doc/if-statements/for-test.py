4537acc8f210f2e9ff239641db45a1f1ec89ad21
sonuk9 <vamsikrishnaceh@gmail.com>
2021-05-06 17:18:09 +0530

    new commit

---
+++ sample-doc/if-statements/for-test.py
@@ -0,0 +1,7 @@
+# printing a multiplication using for loop 
+
+number = int(input("Enter an integer: "))
+for count in range(1, 11):
+     print(count)
+     product = number * count
+     print(number, x )
---
+++ sample-doc/if-statements/test3.py
@@ -0,0 +1,9 @@
+# Python Program to Count Total Characters in a String
+
+str1 = int(input("Enter a string : "))
+total = 0
+
+for i in str:
+    total = total+1
+
+print("Total number of characters in the string =" , total)
