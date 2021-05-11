cfcf806e0c20ff7f5ec54e9493b097b8212e42f6
sonuk9 <vamsikrishnaceh@gmail.com>
2021-05-11 14:16:21 +0530

    new commiy

--- sample-doc/if-statements/multiplication-table.py
+++ sample-doc/if-statements/multiplication-table.py
@@ -1,6 +1,6 @@
-# this python program will provide the multiplication table of a given number
+# this python program will provide the multiplication table of a given number using if statement
 
-num = int(input("Enter your number : "))
+num = int(input("Enter the number : "))
 print("Multiplication table of", num)
-for i in range(1, 21):
+for i in range(1, 11):
     print(num,"x",i,"=", num * i)
